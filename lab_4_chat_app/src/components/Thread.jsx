75930808d077c578e43a1f20536342a9e7b48647
brightenwan <70680054+brightenwan@users.noreply.github.com>
2022-05-09 17:18:31 -0700

    css changes

--- lab_4_chat_app/src/components/Thread.jsx
+++ lab_4_chat_app/src/components/Thread.jsx
@@ -2,6 +2,7 @@ import { useState, useMemo } from "react";
 import Chat from "./Chat";
 import Post from "./Post";
 import { PostContext } from "../contexts/PostContext";
+import "./Thread.css";
 
 function Thread(props) {
 	// every thread should have their own array of posts
@@ -24,8 +25,8 @@ function Thread(props) {
 				{/* Loop through each Thread's array of posts and render them as Chats */}
 				{Object.keys(props).length > 0 && posts.length > 0
 					? posts.map(post => {
-							return <Chat key={post.id} {...post} />;
-					  })
+							return <Chat key={post.id} {...post} />
+					})
 					: null}
 			</PostContext.Provider>
 		</div>
