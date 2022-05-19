d1ca25c84989b96a5feab1d46b72d2d716ef2e29
brightenwan <70680054+brightenwan@users.noreply.github.com>
2022-05-18 19:45:25 -0700

    commited

---
+++ Lab 5 - Server/book-api.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+let books = [];
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+app.post('/book', (req,res) =>{
+    const book = req.body;
+
+    console.log(book);
+    books.push(book);
+
+    // res.send('Book is added to the database');
+});
+app.get('/books', (req,res)=>{
+    res.json(books);
+});
+app.get('/book/:isbn',(req,res)=>{
+    const isbn = req.params.isbn;
+    for(let i = 0; i < books.length; i++){
+        let book = books[i];
+
+        if(book.isbn === isbn){
+            res.json(book);
+        }
+    }
+});
+app.post('/book/:isbn',(req,res) =>{
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+    for (let i = 0; i < books.length; i++){
+        let book = books[i];
+
+        if(book.isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+});
+app.delete('/book/:isbn',(req,res) => {
+    const isbn = req.params.isbn;
+    for (let i = 0; i < books.length; i++){
+        let book = books[i];
+
+        if(book.isbn === isbn){
+            books.splice(i,1);
+        }
+    }
+})
+
+app.listen(port,() => console.log(`Hello world app listening on port`));
