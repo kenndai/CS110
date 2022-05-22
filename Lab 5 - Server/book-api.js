e3bb32d969f5b4d34e763674569395400d70f5e5
brightenwan <70680054+brightenwan@users.noreply.github.com>
2022-05-21 21:32:08 -0700

    added initialization with book.json

--- Lab 5 - Server/book-api.js
+++ Lab 5 - Server/book-api.js
@@ -1,59 +1,60 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
-
-let books = [];
+const initBooks = require('./book.json')
+let books = initBooks;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(express.static('public'))
 
-// get all books
-app.get("/books", (req, res) => {
-	res.json(books);
+app.post('/book', (req,res) =>{
+    const book = req.body;
+
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
-
-// get a book by isbn
-app.get("/book/:isbn", (req, res) => {
-	const { isbn } = req.params;
-	const foundBook = books.find(book => book.isbn === isbn);
-	res.json(foundBook);
+app.get('/books', (req,res)=>{
+    res.json(books);
 });
+app.get('/book/:isbn',(req,res)=>{
+    const isbn = req.params.isbn;
+    for(let i = 0; i < books.length; i++){
+        let book = books[i];
 
-// create new book
-app.post("/book", (req, res) => {
-	const book = req.body;
-
-	console.log("book added");
-	books.push(book);
-
-	res.send("Book is added to the database");
+        if(book.isbn === isbn){
+            res.json(book);
+        }
+    }
 });
+app.post('/book/:isbn',(req,res) =>{
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+    for (let i = 0; i < books.length; i++){
+        let book = books[i];
 
-// update a book
-app.post("/book/:isbn", (req, res) => {
-	const isbn = req.params.isbn;
-	const newBook = req.body;
-
-	for (let i = 0; i < books.length; i++) {
-		let book = books[i];
-		if (book.isbn === isbn) books[i] = newBook;
-	}
-
-	res.send("Book is edited");
+        if(book.isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+	res.send('book changed');
 });
+app.delete('/book/:isbn',(req,res) => {
+    const isbn = req.params.isbn;
+    for (let i = 0; i < books.length; i++){
+        let book = books[i];
 
-app.delete("/book/:isbn", (req, res) => {
-	const { isbn } = req.params;
-	books = books.filter(book => book.isbn !== isbn);
-	res.send("Book was deleted");
-	res.redirect("/books");
-});
+        if(book.isbn === isbn){
+            books.splice(i,1);
+        }
+    }
+})
 
-app.listen(port, () => {
-	console.log(`App is listening on ${port}`);
-});
+app.listen(port,() => console.log(`Hello world app listening on port`));
